7b6035d7d081914b0dd3fe5f6c3262427746093a
tomual <tomual@users.noreply.github.com>
2018-02-08 22:20:17 -0600

    Display hot posts from Reddit

---
+++ src/main/java/com/givemehot/givemehot/GiveMeHotApplication.java
@@ -0,0 +1,12 @@
+package com.givemehot.givemehot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveMeHotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveMeHotApplication.class, args);
+	}
+}
